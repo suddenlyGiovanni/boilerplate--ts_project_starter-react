4edff9d0212f8c100a8c38034d59a0971b770c58
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2019-05-28 20:34:07 +0200

    feat: scaffolded app structure

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { App } from '.'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
