66d1eee7d59b6cf20c53832d267cac5f412fee6d
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2019-06-02 19:22:38 +0200

    feat(package.json): installed react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
 import 'styles/index.css'
 
@@ -10,18 +11,15 @@ import * as serviceWorker from './serviceWorker'
 
 const rootElement = document.getElementById('root')
 
-// const Root: React.FC = () => (
-//   <Provider store={store}>
-//     <AppConnected />
-//   </Provider>
-// )
-
-ReactDOM.render(
+const Root: React.FC = () => (
   <Provider store={store}>
-    <AppConnected />
-  </Provider>,
-  rootElement
+    <BrowserRouter>
+      <AppConnected />
+    </BrowserRouter>
+  </Provider>
 )
+
+ReactDOM.render(<Root />, rootElement)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
