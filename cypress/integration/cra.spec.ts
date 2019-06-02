85fbf5505648d2ccdbb3e9b4c506bba819bb6ffa
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2019-06-02 17:14:16 +0200

    feat(cypress): configured cy to run TS tests

--- cypress/integration/cra.spec.ts
+++ cypress/integration/cra.spec.ts
@@ -1,7 +1,7 @@
 describe('CRA', () => {
   it('shows learn link', () => {
     cy.visit('http://localhost:3000')
-    cy.getByText('Learn React')
+    cy.get('.App-link')
       .should('be.visible')
       .and('have.text', 'Learn React')
   })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -10,8 +11,8 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('file:preprocessor', cypressTypeScriptPreprocessor)
 }
