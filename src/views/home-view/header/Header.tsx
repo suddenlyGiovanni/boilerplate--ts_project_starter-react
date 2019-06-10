14a2d7e3c264c9aadc20c8f2af6f3bb9923e7b39
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2019-06-10 20:09:13 +0200

    refactor(home-view/header): converted css to css-in-js

--- src/views/home-view/header/Header.tsx
+++ src/views/home-view/header/Header.tsx
@@ -1,23 +1,73 @@
 import * as React from 'react'
+import styled from '@emotion/styled'
+import { keyframes } from '@emotion/core'
 
 import logo from 'assets/logo.svg'
-import './Header.css'
+
+const HeaderWrapper = styled.div`
+  text-align: center;
+  width: 100%;
+`
+
+const Link = styled.a`
+  color: #61dafb;
+`
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+
+  /* min-height: 100vh; */
+
+  color: white;
+  background-color: #282c34;
+
+  font-size: calc(10px + 2vmin);
+`
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const Logo = styled.img`
+  height: 40vmin;
+
+  animation: ${rotate} infinite 20s linear;
+  pointer-events: none;
+`
+
+const EditCopy = styled.p`
+  color: white;
+  background-color: #282c34;
+
+  font-size: calc(10px + 2vmin);
+
+  code {
+    color: palegoldenrod;
+  }
+`
 
 export const Header: React.FC = () => (
-  <div className="Header">
-    <header className="Header-header">
-      <img src={logo} className="Header-logo" alt="logo" />
-      <p>
-        Edit <code>src/screens/home</code> and save to reload.
-      </p>
-      <a
-        className="Header-link"
+  <HeaderWrapper>
+    <HeaderContainer>
+      <Logo src={logo} alt="logo" />
+      <EditCopy>
+        Edit <code>src/views/home-view/home</code> and save to reload.
+      </EditCopy>
+      <Link
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
       >
         Learn React
-      </a>
-    </header>
-  </div>
+      </Link>
+    </HeaderContainer>
+  </HeaderWrapper>
 )
